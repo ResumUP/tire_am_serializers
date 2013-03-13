1a1c7bea04818f850d50b681ab39584d73ffbf6b
Sergey Efremov <efremov.sergey@gmail.com>
2013-03-13 17:06:32 +0400

    first working version

--- lib/tire/active_model/serializer.rb
+++ lib/tire/active_model/serializer.rb
@@ -2,14 +2,28 @@ module Tire
   module ActiveModel
     module Serializer
 
-      def serializer_to_json options = {}
-        active_model_serializer.new(self).to_json options
+      def serializer_to_json(options = {})
+        serialization :to_json, options
       end
 
-      def serializer_as_json options={}
-        active_model_serializer.new(self).as_json options
+      def serializer_as_json(options = {})
+        serialization :as_json, options
       end
-      
+
+      private
+        
+      def serializer_defined?
+        !active_model_serializer.nil?
+      end
+
+      def serialization(method, options)
+        if serializer_defined?
+          active_model_serializer.new(self).send(method, options)
+        else
+          send("tire_#{method}", options)
+        end
+      end
+
     end
   end
 end
--- lib/tire/am_serializers.rb
+++ lib/tire/am_serializers.rb
@@ -1,6 +1,8 @@
 require "tire/version"
 require "tire/active_model/serializer"
 
+Tire::Results::Collection.send(:include, ActiveModel::ArraySerializerSupport)
+
 Tire::Results::Item.send(:include, ActiveModel::SerializerSupport)
 Tire::Results::Item.send(:include, Tire::ActiveModel::Serializer)
 
