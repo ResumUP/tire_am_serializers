9e7d512d944f27ecf640c0ee1a44696dbbe290c3
Sergey Efremov <efremov.sergey@gmail.com>
2015-08-24 14:37:02 +0300

    just suport

--- lib/tire/am_serializers.rb
+++ lib/tire/am_serializers.rb
@@ -1,6 +1,6 @@
 require "tire/version"
 require "tire/active_model/serializer_support"
 
-Tire::Results::Collection.send(:include, ActiveModel::ArraySerializerSupport)
+Tire::Results::Collection.send(:include, ActiveModel::SerializerSupport)
 Tire::Results::Item.send(:include, ActiveModel::SerializerSupport)
 Tire::Results::Item.send(:include, Tire::ActiveModel::SerializerSupport)
