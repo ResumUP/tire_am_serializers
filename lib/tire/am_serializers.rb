fb5f4e70eb43aebb3142ec22c31ea7fab470ed89
Sergey Efremov <efremov.sergey@gmail.com>
2015-08-25 13:01:29 +0300

    try rails 4.2 Serialization

--- lib/tire/am_serializers.rb
+++ lib/tire/am_serializers.rb
@@ -1,6 +1,6 @@
 require "tire/version"
 require "tire/active_model/serializer_support"
 
-Tire::Results::Collection.send(:include, ActiveModel::SerializerSupport)
-Tire::Results::Item.send(:include, ActiveModel::SerializerSupport)
+Tire::Results::Collection.send(:include, ActiveModel::Serialization)
+Tire::Results::Item.send(:include, ActiveModel::Serialization)
 Tire::Results::Item.send(:include, Tire::ActiveModel::SerializerSupport)
